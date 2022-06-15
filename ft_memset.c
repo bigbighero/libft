43df47ef9fe3677636bc751bdeebef8d96172cde
roferrei <roferrei@student.42sp.org.br>
2022-06-15 02:41:56 +0200

    add ft_memset.c

---
+++ ft_memset.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: roferrei <roferrei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/03 23:25:57 by roferrei          #+#    #+#             */
+/*   Updated: 2022/06/15 02:41:03 by roferrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	char	*str;
+
+	str = (char *)s;
+	while (n > 0)
+	{
+		str[n - 1] = c;
+		n--;
+	}
+	return (s);
+}
+
+// int	main ()
+// {
+// 	char str[30];
+
+// 	strcpy(str, "this is a test");
+// 	puts(str);
+
+// 	ft_memset(str, '0', 7);
+// 	puts(str);
+
+// 	return (0);
+// }
