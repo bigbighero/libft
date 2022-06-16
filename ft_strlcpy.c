aff36973b1d08eebce506731c5baedea06fd5b1c
roferrei <roferrei@student.42sp.org.br>
2022-06-16 23:07:24 +0200

    add ft_strlcpy

---
+++ ft_strlcpy.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: roferrei <roferrei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/03 23:26:13 by roferrei          #+#    #+#             */
+/*   Updated: 2022/06/10 01:37:30 by roferrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	index;
+	
+	index = 0;
+	if (!size)
+		return (strlen(src));
+	while (index + 1 < size && src[index])
+	{
+		dst[index] = src[index];
+		index++;
+	}
+	dst[index] = '\0';
+	return (strlen(src));
+}
