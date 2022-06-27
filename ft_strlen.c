3e636b449cb89b06f9791ae04896ed30225f13f5
roferrei <roferrei@student.42sp.org.br>
2022-06-28 01:27:21 +0200

    add main in ft_strlen.c

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: roferrei <roferrei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/03 23:26:15 by roferrei          #+#    #+#             */
-/*   Updated: 2022/06/16 23:32:25 by roferrei         ###   ########.fr       */
+/*   Updated: 2022/06/28 01:26:33 by roferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,3 +21,15 @@ size_t	ft_strlen(const char *s)
 		i++;
 	return (i);
 }
+
+// int main (void)
+// {
+// 	char a[20] = "Programa";
+// 	char b[20] = {'P','r','o','g','r','a','m', '\0'};
+// 	printf("Lenght of string a = %zu \n", ft_strlen(a));
+// 	printf("Lenght of string b = %zu \n", ft_strlen(b));
+	
+// 	printf("Lenght of string a = %zu \n", strlen(a));
+// 	printf("Lenght of string b = %zu \n", strlen(b));
+// 	return (0);
+// }
