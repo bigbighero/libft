385f296d1542c8f1a8a787679d69161b1683fb4c
roferrei <tecnicorodrigo@outlook.com>
2022-06-29 00:37:28 +0200

    add main in strlcpy.c

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: roferrei <roferrei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/03 23:26:13 by roferrei          #+#    #+#             */
-/*   Updated: 2022/06/28 01:38:52 by roferrei         ###   ########.fr       */
+/*   Updated: 2022/06/29 00:37:07 by roferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,4 +26,14 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	}
 	dst[i] = '\0';
 	return (strlen(src));
+}
+
+int main (void)
+{
+	printf("\nTeste de ft_strlcpy : \n");
+	char	*c1 = "source";
+	char	c2[] = "destination";
+	char	c3[] = "destination";
+	printf("6, sour : %lu, %s\n", ft_strlcpy(c2, c1, 5), c2);
+	printf("6, source : %lu, %s\n", ft_strlcpy(c3, c1, 8), c3);
 }
