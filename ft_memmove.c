3c8cb96c6284a322f3bcf25f2f7b9381847fc975
roferrei <tecnicorodrigo@outlook.com>
2022-06-30 03:15:32 +0200

    update some files

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: roferrei <roferrei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/03 23:25:55 by roferrei          #+#    #+#             */
-/*   Updated: 2022/06/30 03:14:11 by roferrei         ###   ########.fr       */
+/*   Updated: 2022/06/30 03:15:11 by roferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,8 +47,10 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 // 	printf("Test ft_memmove: \n");
 // 	char s1[] = "string test";
 // 	char s2[] = "string test";
-// 	if(!strcmp(memmove(s1, s1 + 3, 5), ft_memmove(s2, s2 + 3, 5)) && !strcmp(memmove(s1 + 3, s1, 5), ft_memmove(s2 + 3, s2, 5)))
+// 	if(!strcmp(memmove(s1, s1 + 3, 5), ft_memmove(s2, s2 + 3, 5)) 
+//	&& !strcmp(memmove(s1 + 3, s1, 5), ft_memmove(s2 + 3, s2, 5)))
 // 		printf("Ok\n");
 // 	else
-// 		printf("Failed (tests : ft_memmove(s, s + 3, 5) and ft_memmove(s + 3, s, 5).\n");
+// 		printf("Failed (tests : ft_memmove(s, s + 3, 5) 
+//		and ft_memmove(s + 3, s, 5).\n");
 // }
