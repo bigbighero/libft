208a560c60e9af07bd22c04de112aec9a8b8e7b5
roferrei <tecnicorodrigo@outlook.com>
2022-07-01 02:42:33 +0200

    update some files

--- libft.h
+++ libft.h
@@ -6,9 +6,19 @@
 /*   By: roferrei <roferrei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 02:04:59 by roferrei          #+#    #+#             */
-/*   Updated: 2022/07/01 02:23:14 by roferrei         ###   ########.fr       */
+/*   Updated: 2022/07/01 02:42:28 by roferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef LIBFT_H
+# define LIBFT_H
+
 #include <stdlib.h>
 #include <unistd.h>
+
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+size_t	ft_strlen(const char *s)
+
+#endif
